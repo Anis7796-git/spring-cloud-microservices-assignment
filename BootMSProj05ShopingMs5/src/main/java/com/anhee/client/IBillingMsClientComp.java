aebdf9e6ec5108a0eb32dd6278687c953079852d
Anis <Anis@LAPTOP-2SP9MUJP>
2025-05-27 16:13:20 +0530

    Using Feign Client interface IBillingMsClientComp for declarative REST calls to BillingMS microservice

---
+++ BootMSProj05ShopingMs5/src/main/java/com/anhee/client/IBillingMsClientComp.java
@@ -0,0 +1,24 @@
+package com.anhee.client;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
+
+
+@FeignClient("BillingMS")
+public interface IBillingMsClientComp {
+	
+	@GetMapping("/billing-api/bill")
+	public ResponseEntity<String> invokeDoBilling();
+
+}
---
+++ BootMSProj05ShopingMs5/src/test/java/com/anhee/BootMsProj05ShopingMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anhee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootMsProj05ShopingMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
