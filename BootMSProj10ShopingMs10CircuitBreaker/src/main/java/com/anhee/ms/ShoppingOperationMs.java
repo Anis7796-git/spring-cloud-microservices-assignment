c7d1549daa702486ae2d9800b9a6d14647848927
Anis7796-git <anis.sk6834@gmail.com>
2025-05-27 17:12:28 +0530

    Update ShoppingOperationMs.java

--- BootMSProj10ShopingMs10CircuitBreaker/src/main/java/com/anhee/ms/ShoppingOperationMs.java
+++ BootMSProj10ShopingMs10CircuitBreaker/src/main/java/com/anhee/ms/ShoppingOperationMs.java
@@ -42,22 +42,3 @@ public class ShoppingOperationMs {
 	}
 	
 }
-//
-//
-//
-//
-//
-//--
-//BillingMsClientComp.invokeDoBilling() url ****==>  http://LAPTOP-2SP9MUJP:6061   <==***
-//BillingMsClientComp.invokeDoBilling() urlInfo ****==>  http://localhost:6061/billing-api/bill   <==***
-//BillingMsClientComp.invokeDoBilling() response ****==>  <200 OK OK,Bill Amount::36552,[Content-Type:"text/plain;charset=UTF-8", Content-Length:"18", Date:"Mon, 19 May 2025 10:06:26 GMT", Keep-Alive:"timeout=60", Connection:"keep-alive"]>   <==***
-//ShoppingOperationMs.doShopping()****==>  <200 OK OK,Bill Amount::36552,[Content-Type:"text/plain;charset=UTF-8", Content-Length:"18", Date:"Mon, 19 May 2025 10:06:26 GMT", Keep-Alive:"timeout=60", Connection:"keep-alive"]><==***
-//BillingMsClientComp.invokeDoBilling() url ****==>  http://LAPTOP-2SP9MUJP:6061   <==***
-//BillingMsClientComp.invokeDoBilling() urlInfo ****==>  http://LAPTOP-2SP9MUJP:6061/billing-api/bill   <==***
-//BillingMsClientComp.invokeDoBilling() response ****==>  <200 OK OK,Bill Amount::70256,[Content-Type:"text/plain;charset=UTF-8", Content-Length:"18", Date:"Mon, 19 May 2025 10:06:50 GMT", Keep-Alive:"timeout=60", Connection:"keep-alive"]>   <==***
-//ShoppingOperationMs.doShopping()****==>  <200 OK OK,Bill Amount::70256,[Content-Type:"text/plain;charset=UTF-8", Content-Length:"18", Date:"Mon, 19 May 2025 10:06:50 GMT", Keep-Alive:"timeout=60", Connection:"keep-alive"]><==***
-//2025-05-19T15:40:41.580+05:30  INFO 9924 --- [ShopingMS] [rap-executor-%d] c.n.d.s.r.aws.ConfigClusterResolver      : Resolving eureka endpoints via configuration
-//BillingMsClientComp.invokeDoBilling() url ****==>  http://LAPTOP-2SP9MUJP:6061   <==***
-//BillingMsClientComp.invokeDoBilling() urlInfo ****==>  http://LAPTOP-2SP9MUJP:6061/billing-api/bill   <==***
-//BillingMsClientComp.invokeDoBilling() response ****==>  <200 OK OK,Bill Amount::37264,[Content-Type:"text/plain;charset=UTF-8", Content-Length:"18", Date:"Mon, 19 May 2025 10:38:28 GMT", Keep-Alive:"timeout=60", Connection:"keep-alive"]>   <==***
-//ShoppingOperationMs.doShopping()****==>  <200 OK OK,Bill Amount::37264,[Content-Type:"text/plain;charset=UTF-8", Content-Length:"18", Date:"Mon, 19 May 2025 10:38:28 GMT", Keep-Alive:"timeout=60", Connection:"keep-alive"]><==***
